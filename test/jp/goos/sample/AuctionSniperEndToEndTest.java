18084cb4a80f96ce510d8a5725e97db845f282c7
Hidenori Miyoshi <miyohide@gmail.com>
2013-03-23 10:27:38 +0900

    initial commit

---
+++ test/jp/goos/sample/AuctionSniperEndToEndTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.goos.sample;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author miyohide
+ */
+public class AuctionSniperEndToEndTest {
+    
+    public AuctionSniperEndToEndTest() {
+    }
+}
