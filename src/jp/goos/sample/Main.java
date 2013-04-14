eac65ed0c9886ebcdef345fe6ef2fd1390f5e253
Hidenori Miyoshi <miyohide@gmail.com>
2013-04-14 16:26:01 +0900

    図11-2の実装までを完了

---
+++ src/jp/goos/sample/Main.java
@@ -0,0 +1,25 @@
+package jp.goos.sample;
+
+import javax.swing.SwingUtilities;
+import jp.goos.sample.ui.MainWindow;
+
+public class Main {
+    private MainWindow ui;
+    
+    public Main() throws Exception {
+        startUserInterface();
+    }
+    
+    public static void main(String... args) throws Exception {
+        Main main = new Main();
+    }
+    
+    private void startUserInterface() throws Exception {
+        SwingUtilities.invokeAndWait(new Runnable() {
+            public void run() {
+                ui = new MainWindow();
+            }
+        });
+    }
+    
+}
