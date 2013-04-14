5f4b97e742c1119b94c8c73eede9ae1dc3b2f443
Hidenori Miyoshi <miyohide@gmail.com>
2013-04-14 16:33:14 +0900

    図11-3の実装まで完了

--- src/jp/goos/sample/ui/MainWindow.java
+++ src/jp/goos/sample/ui/MainWindow.java
@@ -1,16 +1,30 @@
 package jp.goos.sample.ui;
 
+import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
 
 public class MainWindow extends JFrame {
     public static final String SNIPER_STATUS_NAME = "sniper status";
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
     
+    private final JLabel sniperStatus = createLabel("joining");
+    
     public MainWindow() {
         super("Auction Sniper");
         setName(MAIN_WINDOW_NAME);
+        add(sniperStatus);
+        pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+    
+    private static JLabel createLabel(String initialText) {
+        JLabel result = new JLabel(initialText);
+        result.setName(SNIPER_STATUS_NAME);
+        result.setBorder(new LineBorder(Color.BLACK));
+        return result;
     }
     
 }
