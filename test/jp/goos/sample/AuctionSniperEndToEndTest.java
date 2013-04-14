682d7606f11336e3d189fb8f5cf8bba48c44ed76
Hidenori Miyoshi <miyohide@gmail.com>
2013-04-14 15:29:37 +0900

    テストの追加。GOOS 99ページまで

--- test/jp/goos/sample/AuctionSniperEndToEndTest.java
+++ test/jp/goos/sample/AuctionSniperEndToEndTest.java
@@ -1,18 +1,28 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package jp.goos.sample;
 
+import org.junit.After;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author miyohide
- */
 public class AuctionSniperEndToEndTest {
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
     
-    public AuctionSniperEndToEndTest() {
+    @Test public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+        auction.startSellingItem();
+        application.startBinddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper();
+        auction.announceClosed();
+        application.showsSniperHasLostAuction();
     }
+    
+    // 追加のクリーンアップ
+    @After public void stopAuction() {
+        auction.stop();
+    }
+    
+    @After public void stopApplication() {
+        application.stop();
+    }
+
 }
