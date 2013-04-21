dda29c21ca596fdc5db871158e3ed204d488c081
Hidenori Miyoshi <miyohide@gmail.com>
2013-04-21 11:27:44 +0900

    P121 unitテストを通るようにした

--- src/jp/goos/sample/AuctionMessageTranslator.java
+++ src/jp/goos/sample/AuctionMessageTranslator.java
@@ -6,7 +6,12 @@ import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.packet.Message;
 
 public class AuctionMessageTranslator implements MessageListener {
+    private AuctionEventListener listener;
+
+    public AuctionMessageTranslator(AuctionEventListener listener) {
+        this.listener = listener;
+    }
     public void processMessage(Chat chat, Message message) {
-        // TODO: 実装すること
+        this.listener.auctionClosed();
     }
 }
