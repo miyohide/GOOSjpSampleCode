7e42eb0f889a68e2c199e89ad68c9c93f5bcd26e
Hidenori Miyoshi <miyohide@gmail.com>
2013-04-21 11:18:40 +0900

    Openfireの設定変更を実施し、サンプルと合わせるようにした

--- test/end-to-end/test/endtoend/auctionsniper/FakeAuctionServer.java
+++ test/end-to-end/test/endtoend/auctionsniper/FakeAuctionServer.java
@@ -20,7 +20,7 @@ import org.jivesoftware.smack.packet.Message;
 public class FakeAuctionServer {
     public static final String ITEM_ID_AS_LOGIN = "auction-%s";
     public static final String AUCTION_RESOURCE = "Auction";
-    public static final String XMPP_HOSTNAME = "tsubame.local"; // Openfireの設定と合わせる。
+    public static final String XMPP_HOSTNAME = "localhost";
     private static final String AUCTION_PASSWORD = "auction";
     
     private final String itemId;
