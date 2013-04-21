dba2898d66cb53900fb47de98f3d836224f41453
Hidenori Miyoshi <miyohide@gmail.com>
2013-04-21 10:48:08 +0900

    P119 notfiesAuctionClosedWhenCloseMessageReceivedテストの実装。AuctionMessageTranslatorの実装前

--- test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
+++ test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
@@ -1,10 +1,18 @@
 package test.auctionsniper;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.packet.Message;
 import org.junit.Test;
 
 public class AuctionMessageTranslatorTest {
+    public static final Chat UNUSED_CHAT = null;
+    private final AuctionMessageTranslator translator = new AuctionMessageTranslator();
+    
     @Test public void notifiesAuctionClosedWhenCloseMessagereceived() {
-        // まだ何もしない
+        Message message = new Message();
+        message.setBody("SOLVersion: 1.1; Event: CLOSE;");
+        
+        translator.processMessage(UNUSED_CHAT, message);
     }
     
 }
