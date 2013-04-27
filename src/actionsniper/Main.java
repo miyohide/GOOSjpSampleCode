1153e28ee3179664d0f7da9e7d4274954a76840c
Hidenori Miyoshi <miyohide@gmail.com>
2013-04-27 19:19:20 +0900

    P134 エンドツーエンドが通るようにした

--- src/actionsniper/Main.java
+++ src/actionsniper/Main.java
@@ -11,63 +11,72 @@ import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
 
 public class Main implements SniperListener {
-    @SuppressWarnings("unused") private Chat notToBeGCd;
-    
+
+    @SuppressWarnings("unused")
+    private Chat notToBeGCd;
     private static final int ARG_HOSTNAME = 0;
     private static final int ARG_USERNAME = 1;
     private static final int ARG_PASSWORD = 2;
     private static final int ARG_ITEM_ID = 3;
-    
     public static final String AUCTION_RESOURCE = "Auction";
     public static final String ITEM_ID_AS_LOGIN = "auction-%s";
     public static final String AUCTION_ID_FORMAT =
             ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
     public static final String JOIN_COMMAND_FORMAT = "SOLVersion: 1.1; Command: JOIN;";
     public static final String BID_COMMAND_FORMAT = "SOLVersion: 1.1; Command: BID; Price: %d;";
-    
     private MainWindow ui;
-    
+
     public Main() throws Exception {
         startUserInterface();
     }
-    
+
     public static void main(String... args) throws Exception {
         Main main = new Main();
         main.joinAuction(
                 connection(args[ARG_HOSTNAME], args[ARG_USERNAME],
-                           args[ARG_PASSWORD]), args[ARG_ITEM_ID]);
+                args[ARG_PASSWORD]), args[ARG_ITEM_ID]);
     }
 
     private void joinAuction(XMPPConnection connection, String itemId) throws XMPPException {
         // ウィンドウをクローズしたとき、クライアントとの接続も切る
-
-        /* コンパイルを通すためにAuctionを実装したクラスを作成し、そのインスタンスを
-         * AuctionSniperの引数に使用する
-         */
-        Auction nullAuction = new Auction() {
-            public void bid(int amount) {}
-        };
         disconnectWhenUICloses(connection);
+
+        /*
+         * chatオブジェクトを生成するときに、MessageListenerは必要ではない。
+         * 生成時にはnullを指定しておき、後でaddMessageListenerでMessageListenerを
+         * 追加する。
+         */
         final Chat chat = connection.getChatManager().createChat(
-                auctionId(itemId, connection),
-                new AuctionMessageTranslator(new AuctionSniper(nullAuction, this)));
+                auctionId(itemId, connection), null);
         this.notToBeGCd = chat;
+
+        Auction auction = new Auction() {
+            public void bid(int amount) {
+                try {
+                    chat.sendMessage(String.format(BID_COMMAND_FORMAT, amount));
+                } catch (XMPPException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        chat.addMessageListener(
+                new AuctionMessageTranslator(new AuctionSniper(auction, this)));
         chat.sendMessage(JOIN_COMMAND_FORMAT);
-     }
-    
-    private static XMPPConnection
-            connection(String hostname, String username, String password) throws XMPPException {
+    }
+
+    private static XMPPConnection connection(String hostname, String username, String password) throws XMPPException {
         XMPPConnection connection = new XMPPConnection(hostname);
         connection.connect();
         connection.login(username, password, AUCTION_RESOURCE);
-        
+
         return connection;
     }
-    
+
     private static String auctionId(String itemId, XMPPConnection connection) {
         return String.format(AUCTION_ID_FORMAT, itemId, connection.getServiceName());
     }
-    
+
     private void startUserInterface() throws Exception {
         SwingUtilities.invokeAndWait(new Runnable() {
             public void run() {
@@ -75,10 +84,11 @@ public class Main implements SniperListener {
             }
         });
     }
-    
+
     private void disconnectWhenUICloses(final XMPPConnection connection) {
         ui.addWindowListener(new WindowAdapter() {
-            @Override public void windowClosed(WindowEvent e) {
+            @Override
+            public void windowClosed(WindowEvent e) {
                 connection.disconnect();
             }
         });
@@ -101,5 +111,4 @@ public class Main implements SniperListener {
             }
         });
     }
-
 }
