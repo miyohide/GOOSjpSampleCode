893bac150015f0bb80a0f2245a9b4cbc0572a360
Hidenori Miyoshi <miyohide@gmail.com>
2013-05-04 15:43:14 +0900

    P149-150 スナイパーに状態を加える。まずは落札失敗のパターン

--- test/unit/test/auctionsniper/AuctionSniperTest.java
+++ test/unit/test/auctionsniper/AuctionSniperTest.java
@@ -6,6 +6,7 @@ import actionsniper.AuctionSniper;
 import actionsniper.SniperListener;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.States;
 import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,14 +17,26 @@ public class AuctionSniperTest {
     private final SniperListener sniperListener = context.mock(SniperListener.class);
     private final Auction auction = context.mock(Auction.class);
     private final AuctionSniper sniper = new AuctionSniper(auction, sniperListener);
+    private final States sniperState = context.states("sniper");
     
-    @Test public void reportsLostWhenAuctionCloses() {
+    @Test public void reportsLostIfAuctionClosesImmediately() {
         context.checking(new Expectations() {{
             one(sniperListener).sniperLost();
         }});
         sniper.auctionClosed();
     }
     
+    @Test public void reportsLostIfAuctionClosesWhenBidding() {
+        context.checking(new Expectations() {{
+            ignoring(auction);
+            allowing(sniperListener).sniperBidding();   then(sniperState.is("bidding"));
+            atLeast(1).of(sniperListener).sniperLost(); when(sniperState.is("bidding"));
+        }});
+
+        sniper.currentPrice(123, 45, PriceSource.FromOtherBidder);
+        sniper.auctionClosed();
+    }
+
     @Test public void bidsHigherAndReportsBiddingWhenNewPriceArrives() {
         final int price = 1001;
         final int increment = 25;
