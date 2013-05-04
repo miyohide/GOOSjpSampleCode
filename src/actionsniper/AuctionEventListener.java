79568963f6cbbd771ef7fd212bae7ab118d8c1a7
Hidenori Miyoshi <miyohide@gmail.com>
2013-05-04 13:47:14 +0900

    P145 PriceSourceの導入

--- src/actionsniper/AuctionEventListener.java
+++ src/actionsniper/AuctionEventListener.java
@@ -1,6 +1,10 @@
 package actionsniper;
 
 public interface AuctionEventListener {
+    enum PriceSource {
+        FromSniper,      // スナイパーから入札
+        FromOtherBidder; // 他の人から入札
+    }
     public void auctionClosed();
     public void currentPrice(int price, int increment);
 }
