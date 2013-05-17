ffa00385b012b0e665afe166de489b91386eb051
Hidenori Miyoshi <miyohide@gmail.com>
2013-05-17 21:12:50 +0900

    P156 JLabelをJTableに置き換え。テストの変更

--- test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -1,10 +1,12 @@
 package test.endtoend.auctionsniper;
 
 import static org.hamcrest.Matchers.equalTo;
+import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 
 import com.objogate.wl.swing.AWTEventQueueProber;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 import jp.goos.sample.ui.MainWindow;
 
@@ -18,7 +20,6 @@ public class AuctionSniperDriver extends JFrameDriver {
     }
     
     public void showsSniperStatus(String statusText) {
-        new JLabelDriver(
-                this, named(MainWindow.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+        new JTableDriver(this).hasCell(withLabelText(equalTo(statusText)));
     }
 }
