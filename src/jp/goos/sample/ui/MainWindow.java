5c6c2e164666e8d2506264709d36283d7f788a7f
Hidenori Miyoshi <miyohide@gmail.com>
2013-05-17 21:56:09 +0900

    使われていない変数を削除

--- src/jp/goos/sample/ui/MainWindow.java
+++ src/jp/goos/sample/ui/MainWindow.java
@@ -19,7 +19,6 @@ public class MainWindow extends JFrame {
     public static final String STATUS_BIDDING = "bidding";
     public static final String STATUS_WINNING = "winning";
     public static final String STATUS_WON = "won";
-    private final JLabel sniperStatus = createLabel("joining");
     private final SnipersTableModel snipers = new SnipersTableModel();
 
     public MainWindow() {
