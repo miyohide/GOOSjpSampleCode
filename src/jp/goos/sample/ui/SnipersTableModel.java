c848824f64a3ee03d50c68c5c43e78ac2eefd911
Hidenori Miyoshi <miyohide@gmail.com>
2013-05-18 14:22:26 +0900

    P163 テストの追加とコンパイルを通すために必要な実装の追加

--- src/jp/goos/sample/ui/SnipersTableModel.java
+++ src/jp/goos/sample/ui/SnipersTableModel.java
@@ -1,17 +1,25 @@
 package jp.goos.sample.ui;
 
+import actionsniper.SniperState;
 import javax.swing.table.AbstractTableModel;
 
-class SnipersTableModel extends AbstractTableModel {
+public class SnipersTableModel extends AbstractTableModel {
     private String statusText = MainWindow.STATUS_JOINING;
     
+    @Override
     public int getColumnCount() { return 1; }
+    @Override
     public int getRowCount() { return 1; }
+    @Override
     public Object getValueAt(int rowIndex, int columnIndex) { return statusText; }
     
     public void setStatusText(String newStatusText) {
         statusText = newStatusText;
         fireTableRowsUpdated(0, 0);
+    }
+
+    public void sniperStatusChanged(SniperState sniperState, String STATUS_BIDDING) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
