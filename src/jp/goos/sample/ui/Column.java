aa21ef16ea16f0301e7eae16218da2b2b32e5b04
Hidenori Miyoshi <miyohide@gmail.com>
2013-05-19 16:27:09 +0900

    P173-174 Columnクラスの拡充とSnipersTableModelでのgetValueAtメソッドの簡素化。

--- src/jp/goos/sample/ui/Column.java
+++ src/jp/goos/sample/ui/Column.java
@@ -1,14 +1,39 @@
 package jp.goos.sample.ui;
 
 // テーブル内のカラムを表現する
+
+import actionsniper.SniperSnapshot;
+
 public enum Column {
-    ITEM_IDENTIFIER,
-    LAST_PRICE,
-    LAST_BID,
-    SNIPER_STATE;
+    ITEM_IDENTIFIER {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.itemId;
+        }
+    },
+    LAST_PRICE {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.lastPrice;
+        }
+    },
+    LAST_BID {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.lastBid;
+        }
+    },
+    SNIPER_STATE {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            // SnipersTableModelの外からステータスのテキスト文言を得るためにtextForをstatic宣言にしている
+            return SnipersTableModel.textFor(snapshot.state);
+        }
+    };
     
     public static Column at(int offset) {
         return values()[offset];
     }
 
+    abstract public Object valueIn(SniperSnapshot snapshot);
 }
