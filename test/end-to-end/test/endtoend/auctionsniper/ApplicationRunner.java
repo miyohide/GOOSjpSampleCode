281b31c9d454a68a5a0903241200a34f59634652
Hidenori Miyoshi <miyohide@gmail.com>
2013-05-23 22:51:29 +0900

    P182-183 複数商品のテストの追加。テキストと異なり、P183のauctionごとにshowsSniperStatusを呼び出す処理をコメント化。これによってテキストと同じ結果が得られる。

--- test/end-to-end/test/endtoend/auctionsniper/ApplicationRunner.java
+++ test/end-to-end/test/endtoend/auctionsniper/ApplicationRunner.java
@@ -1,4 +1,5 @@
 package test.endtoend.auctionsniper;
+
 import actionsniper.Main;
 import actionsniper.SniperState;
 import static test.endtoend.auctionsniper.FakeAuctionServer.XMPP_HOSTNAME;
@@ -11,15 +12,13 @@ public class ApplicationRunner {
     public static final String SNIPER_PASSWORD = "sniper";
     public static final String SNIPER_XMPP_ID = SNIPER_ID + "@" + XMPP_HOSTNAME + "/Auction";
     private AuctionSniperDriver driver;
-    private String itemId;
-    
-    public void startBinddingIn(final FakeAuctionServer auction) {
-        itemId = auction.getItemId();
+
+    public void startBinddingIn(final FakeAuctionServer... auctions) {
         Thread thread = new Thread("Test Application") {
             @Override
             public void run() {
                 try {
-                    Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId());
+                    Main.main(arguments(auctions));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -30,18 +29,31 @@ public class ApplicationRunner {
         driver = new AuctionSniperDriver(1000);
         driver.hasTitle(MainWindow.APPLICATION_TITLE);
         driver.hasColumnTitles();
-        driver.showsSniperStatus(
-                SnipersTableModel.textFor(SniperState.JOINING));
+//        for (FakeAuctionServer auction : auctions) {
+//            driver.showsSniperStatus(auction.getItemId(), 0, 0,
+//                    SnipersTableModel.textFor(SniperState.JOINING));
+//        }
     }
-    
+
+    protected static String[] arguments(FakeAuctionServer... auctions) {
+        String[] arguments = new String[auctions.length + 3];
+        arguments[0] = XMPP_HOSTNAME;
+        arguments[1] = SNIPER_ID;
+        arguments[2] = SNIPER_PASSWORD;
+        for (int i = 0; i < auctions.length; i++) {
+            arguments[i + 3] = auctions[i].getItemId();
+        }
+        return arguments;
+    }
+
     public void showsSniperHasLostAuction() {
         driver.showsSniperStatus(SnipersTableModel.STATUS_LOST);
     }
-    
+
     public void showsSniperHasWonAuction() {
         driver.showsSniperStatus(SnipersTableModel.STATUS_WINNING);
     }
-    
+
     public void stop() {
         if (driver != null) {
             driver.dispose();
@@ -52,19 +64,19 @@ public class ApplicationRunner {
         driver.showsSniperStatus(SnipersTableModel.STATUS_BIDDING);
     }
 
-    public void hasShownSniperIsBidding(int lastPrice, int lastBid) {
-        driver.showsSniperStatus(itemId, lastPrice, lastBid, SnipersTableModel.STATUS_BIDDING);
+    public void hasShownSniperIsBidding(FakeAuctionServer auction, int lastPrice, int lastBid) {
+        driver.showsSniperStatus(auction.getItemId(), lastPrice, lastBid, SnipersTableModel.STATUS_BIDDING);
     }
-    
+
     void hasShownSniperIsWinning() {
         driver.showsSniperStatus(SnipersTableModel.STATUS_WINNING);
     }
 
-    public void hasShownSniperIsWinning(int winningBid) {
-        driver.showsSniperStatus(itemId, winningBid, winningBid, SnipersTableModel.STATUS_WINNING);
+    public void hasShownSniperIsWinning(FakeAuctionServer auction, int winningBid) {
+        driver.showsSniperStatus(auction.getItemId(), winningBid, winningBid, SnipersTableModel.STATUS_WINNING);
     }
 
-    public void showsSniperHasWonAuction(int lastPrice) {
-        driver.showsSniperStatus(itemId, lastPrice, lastPrice, SnipersTableModel.STATUS_WON);
+    public void showsSniperHasWonAuction(FakeAuctionServer auction, int lastPrice) {
+        driver.showsSniperStatus(auction.getItemId(), lastPrice, lastPrice, SnipersTableModel.STATUS_WON);
     }
 }
--- test/end-to-end/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ test/end-to-end/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -5,10 +5,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AuctionSniperEndToEndTest {
+
     private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final FakeAuctionServer auction2 = new FakeAuctionServer("item-65432");
     private final ApplicationRunner application = new ApplicationRunner();
-    
-    @Test public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+
+    @Test
+    public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
         auction.startSellingItem();
         application.startBinddingIn(auction);
         // メソッドシグネチャの変更に応じて、ここも修正（P113）
@@ -16,48 +19,79 @@ public class AuctionSniperEndToEndTest {
         auction.announceClosed();
         application.showsSniperHasLostAuction();
     }
-    
-    @Test public void sniperMakesAHigherBidButLoses() throws Exception {
+
+    @Test
+    public void sniperMakesAHigherBidButLoses() throws Exception {
         auction.startSellingItem();
-        
+
         application.startBinddingIn(auction);
         auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
-        
+
         auction.reportPrice(1000, 98, "other bidder");
-        
+
         application.hasShownSniperIsBidding();
         auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
-        
+
         auction.announceClosed();
         application.showsSniperHasLostAuction();
     }
-    
-    @Test public void sniperWinsAnAuctionByBiddingHigher() throws Exception {
+
+    @Test
+    public void sniperWinsAnAuctionByBiddingHigher() throws Exception {
         auction.startSellingItem();
-        
+
         application.startBinddingIn(auction);
         auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
-        
+
         auction.reportPrice(1000, 98, "other bidder");
         // 最新価格、最新入札額
-        application.hasShownSniperIsBidding(1000, 1098);
+        application.hasShownSniperIsBidding(auction, 1000, 1098);
 
         auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
-        
+
         auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
-        application.hasShownSniperIsWinning(1098); // 落札時の入札額
-        
+        application.hasShownSniperIsWinning(auction, 1098); // 落札時の入札額
+
         auction.announceClosed();
-        application.showsSniperHasWonAuction(1098); // 最新価格
-    }
-    
-    // 追加のクリーンアップ
-    @After public void stopAuction() {
-        auction.stop();
-    }
-    
-    @After public void stopApplication() {
-        application.stop();
+        application.showsSniperHasWonAuction(auction, 1098); // 最新価格
     }
 
+    @Test
+    public void sniperBidsForMultipleItems() throws Exception {
+        auction.startSellingItem();
+        auction2.startSellingItem();
+
+        application.startBinddingIn(auction, auction2);
+        auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+        auction2.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+
+        auction.reportPrice(1000, 98, "other bidder");
+        auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
+
+        auction2.reportPrice(500, 21, "other bidder");
+        auction2.hasReceivedBid(521, ApplicationRunner.SNIPER_XMPP_ID);
+
+        auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
+        auction2.reportPrice(521, 22, ApplicationRunner.SNIPER_XMPP_ID);
+
+        application.hasShownSniperIsWinning(auction, 1098);
+        application.hasShownSniperIsWinning(auction2, 521);
+
+        auction.announceClosed();
+        auction2.announceClosed();
+
+        application.showsSniperHasWonAuction(auction, 1098);
+        application.showsSniperHasWonAuction(auction2, 1098);
+    }
+
+    // 追加のクリーンアップ
+    @After
+    public void stopAuction() {
+        auction.stop();
+    }
+
+    @After
+    public void stopApplication() {
+        application.stop();
+    }
 }
