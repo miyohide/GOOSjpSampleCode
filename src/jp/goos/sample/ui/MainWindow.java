8ea8ff4901257b27be38750af63f4b0c4824ddbc
Hidenori Miyoshi <miyohide@gmail.com>
2013-05-26 12:41:39 +0900

    P192 アクションバーの追加。この状態でテストを動かすと、windowlickerがMac-JPを認識しないため、異常終了する。

--- src/jp/goos/sample/ui/MainWindow.java
+++ src/jp/goos/sample/ui/MainWindow.java
@@ -4,10 +4,14 @@ import actionsniper.SniperSnapshot;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
 public class MainWindow extends JFrame {
@@ -18,27 +22,26 @@ public class MainWindow extends JFrame {
     public static final String APPLICATION_TITLE = "Auction Sniper";
     public static final String NEW_ITEM_ID_NAME = "item id";
     public static final String JOIN_BUTTON_NAME = "join button";
-    private SnipersTableModel snipers;
 
     public MainWindow(SnipersTableModel sniper) {
-        super("Auction Sniper");
-        this.snipers = sniper;
+        super(APPLICATION_TITLE);
         setName(MAIN_WINDOW_NAME);
-        fillContentPane(makeSnipersTable());
+        fillContentPane(makeSnipersTable(sniper), makeControls());
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-    private void fillContentPane(JTable snipersTable) {
+    private void fillContentPane(JTable snipersTable, JPanel controls) {
         final Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
 
+        contentPane.add(controls, BorderLayout.NORTH);
         contentPane.add(new JScrollPane(snipersTable), BorderLayout.CENTER);
     }
 
-    private JTable makeSnipersTable() {
-        final JTable snipersTable = new JTable(snipers);
+    private JTable makeSnipersTable(SnipersTableModel sniper) {
+        final JTable snipersTable = new JTable(sniper);
         snipersTable.setName(SNIPERS_TABLE_NAME);
         return snipersTable;
     }
@@ -50,7 +53,23 @@ public class MainWindow extends JFrame {
         return result;
     }
 
-    public void sniperStatusChanged(SniperSnapshot sniperSnapshot) {
-        snipers.sniperStateChanged(sniperSnapshot);
+    private JPanel makeControls() {
+        JPanel controls = new JPanel(new FlowLayout());
+        // 商品IDのためのテキストフィールド
+        final JTextField itemIdField = new JTextField();
+        itemIdField.setColumns(25);
+        itemIdField.setName(NEW_ITEM_ID_NAME);
+        controls.add(itemIdField);
+
+        // 「Join Auction」ボタン
+        JButton joinAuctionButton = new JButton("Join Auction");
+        joinAuctionButton.setName(JOIN_BUTTON_NAME);
+        controls.add(joinAuctionButton);
+
+        return controls;
     }
+
+//    public void sniperStatusChanged(SniperSnapshot sniperSnapshot) {
+//        snipers.sniperStateChanged(sniperSnapshot);
+//    }
 }
