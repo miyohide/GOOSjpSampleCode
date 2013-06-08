d3881f94394ca36e2c41b86866a1ca7bc8408dfb
Hidenori Miyoshi <miyohide@gmail.com>
2013-06-08 10:36:23 +0900

    P203 XMPPAuctionに対する新しいテストの追加。パッケージの移動はまだ

---
+++ test/integration/test/integration/auctionsniper/xmpp/XMPPAuctionTest.java
@@ -0,0 +1,43 @@
+package test.integration.auctionsniper.xmpp;
+
+import static org.junit.Assert.assertTrue;
+
+import actionsniper.Auction;
+import actionsniper.AuctionEventListener;
+import actionsniper.AuctionEventListener.PriceSource;
+import actionsniper.XMPPAuction;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import org.jivesoftware.smack.XMPPConnection;
+import org.junit.Test;
+import test.endtoend.auctionsniper.ApplicationRunner;
+import test.endtoend.auctionsniper.FakeAuctionServer;
+
+public class XMPPAuctionTest {
+    @Test public void receivesEventsFromAuctionServerAfterJoining() throws Exception {
+        XMPPConnection connection = new XMPPConnection("localhost");
+        connection.connect();
+        connection.login(ApplicationRunner.SNIPER_ID, ApplicationRunner.SNIPER_PASSWORD, FakeAuctionServer.AUCTION_RESOURCE);
+        FakeAuctionServer auctionServer = new FakeAuctionServer("item-54321");
+        auctionServer.startSellingItem();
+        CountDownLatch auctionWasClosed = new CountDownLatch(1);
+        
+        Auction auction = new XMPPAuction(connection, auctionServer.getItemId());
+        auction.addAuctionEventListener(auctionClosedListener(auctionWasClosed));
+        
+        auction.join();
+        auctionServer.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
+        auctionServer.announceClosed();
+        
+        assertTrue("should have been closed", auctionWasClosed.await(2, TimeUnit.SECONDS));
+    }
+    
+    private AuctionEventListener auctionClosedListener(final CountDownLatch auctionWasClosed) {
+        return new AuctionEventListener() {
+            public void auctionClosed() { auctionWasClosed.countDown(); }
+            public void currentPrice(int price, int increment, PriceSource priceSource) {
+                // 未実装
+            }
+        };
+    }
+}
