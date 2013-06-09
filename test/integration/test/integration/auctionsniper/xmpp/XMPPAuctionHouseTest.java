16b809606ddb8e81fff60505fee08e09a3cffb18
Hidenori Miyoshi <miyohide@gmail.com>
2013-06-09 16:32:52 +0900

    P205 テストの変更とファイル名の変更

--- test/integration/test/integration/auctionsniper/xmpp/XMPPAuctionHouseTest.java
+++ test/integration/test/integration/auctionsniper/xmpp/XMPPAuctionHouseTest.java
@@ -5,24 +5,23 @@ import static org.junit.Assert.assertTrue;
 import auctionsniper.Auction;
 import auctionsniper.AuctionEventListener;
 import auctionsniper.AuctionEventListener.PriceSource;
-import auctionsniper.xmpp.XMPPAuction;
+import auctionsniper.xmpp.XMPPAuctionHouse;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import org.jivesoftware.smack.XMPPConnection;
 import org.junit.Test;
 import test.endtoend.auctionsniper.ApplicationRunner;
 import test.endtoend.auctionsniper.FakeAuctionServer;
 
-public class XMPPAuctionTest {
+public class XMPPAuctionHouseTest {
     @Test public void receivesEventsFromAuctionServerAfterJoining() throws Exception {
-        XMPPConnection connection = new XMPPConnection("localhost");
-        connection.connect();
-        connection.login(ApplicationRunner.SNIPER_ID, ApplicationRunner.SNIPER_PASSWORD, FakeAuctionServer.AUCTION_RESOURCE);
+        XMPPAuctionHouse auctionHouse = XMPPAuctionHouse.connect("localhost",
+                ApplicationRunner.SNIPER_ID, ApplicationRunner.SNIPER_PASSWORD);
+
         FakeAuctionServer auctionServer = new FakeAuctionServer("item-54321");
         auctionServer.startSellingItem();
         CountDownLatch auctionWasClosed = new CountDownLatch(1);
         
-        Auction auction = new XMPPAuction(connection, auctionServer.getItemId());
+        Auction auction = auctionHouse.auctionFor("item-54321");
         auction.addAuctionEventListener(auctionClosedListener(auctionWasClosed));
         
         auction.join();
