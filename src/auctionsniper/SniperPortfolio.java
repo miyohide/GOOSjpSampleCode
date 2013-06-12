bc4c1cc5a4e520c50eaba78b34b94160d164e970
Hidenori Miyoshi <miyohide@gmail.com>
2013-06-12 21:37:09 +0900

    P208 PortfolioListenerインタフェースとSniperPortfolioクラスを作成。現在は空実装

---
+++ src/auctionsniper/SniperPortfolio.java
@@ -0,0 +1,8 @@
+package auctionsniper;
+
+public class SniperPortfolio implements PortfolioListener {
+    @Override
+    public void sniperAdded(AuctionSniper sniper) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
