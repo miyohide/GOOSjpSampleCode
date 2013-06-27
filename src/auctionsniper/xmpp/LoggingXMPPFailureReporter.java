932b860fe0504df8294fd806d3bd879fa6059378
Hidenori Miyoshi <miyohide@gmail.com>
2013-06-27 23:42:07 +0900

    P232 LoggingXMPPFailureReporterのテストを書く。テストのコンパイルエラー対策のため、コンストラクタを追加。

--- src/auctionsniper/xmpp/LoggingXMPPFailureReporter.java
+++ src/auctionsniper/xmpp/LoggingXMPPFailureReporter.java
@@ -1,6 +1,11 @@
 package auctionsniper.xmpp;
 
+import java.util.logging.Logger;
+
 public class LoggingXMPPFailureReporter implements XMPPFailureReporter {
+
+    public LoggingXMPPFailureReporter(Logger logger) {
+    }
 
     @Override
     public void cannotTranslateMessage(String auctionId, String failedMessage, Exception exception) {
