6c8e7f52b962c3a6ff33cddaad124ff9169a6694
Hidenori Miyoshi <miyohide@gmail.com>
2013-06-28 23:35:42 +0900

    LoggingXMPPFailureReporterTestが通るように実装

--- src/auctionsniper/xmpp/LoggingXMPPFailureReporter.java
+++ src/auctionsniper/xmpp/LoggingXMPPFailureReporter.java
@@ -4,11 +4,16 @@ import java.util.logging.Logger;
 
 public class LoggingXMPPFailureReporter implements XMPPFailureReporter {
 
+    final private Logger logger;
+
     public LoggingXMPPFailureReporter(Logger logger) {
+        this.logger = logger;
     }
 
     @Override
     public void cannotTranslateMessage(String auctionId, String failedMessage, Exception exception) {
+        logger.severe("<" + auctionId + "> Could not translate message \"" +
+                failedMessage + "\" because \"" + exception + "\"");
     }
     
 }
